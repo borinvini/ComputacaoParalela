53f0a9e857ee76378bc69ec0fb4a1f9676b57ada
borinvini <borin.vini@gmail.com>
2019-04-09 17:38:41 -0300

    Add files via upload

---
+++ 2-integral/IntegralParallel_v4/IntegralParallel_v4/Origem.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <time.h>
+
+static long num_steps = 100000000;
+double step;
+
+int main()
+{
+	int i;
+	double pi, sum = 0.0;
+
+	step = 1.0 / (double)num_steps;
+	double start_time = omp_get_wtime(); //calcula tempo
+
+//#pragma omp parallel for reduction(+:sum) private(x)
+#pragma omp parallel for reduction(+:sum)
+	for (i = 0; i < num_steps; i++)
+	{
+		double x;
+		x = (i + 0.5)*step;
+		sum = sum + 4.0 / (1.0 + x*x);
+	}
+
+	
+	pi = sum * step;
+
+	double end_time = omp_get_wtime(); //calcula tempo
+
+	printf("%f\n", pi);
+	printf("%lf s\n", end_time - start_time);
+	system("pause");
+	return 0;
+}
